2a8305d98d52dd720159df986822a04a60f5df47
qwq-nm <3238764559@qq.com>
2026-04-07 13:06:08 +0800

    Move static resources to public directory for Vercel deployment

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ from app import app, db
 # 添加静态文件路由
 @app.route('/static/<path:path>')
 def send_static(path):
-    return send_from_directory('app/static', path)
+    return send_from_directory('public', path)
 
 # 导入Markdown解析库
 import mistune
